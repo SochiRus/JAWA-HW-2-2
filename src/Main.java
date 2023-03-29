868fa61509ceaee7b9de9a3ebeb0858e21d1888e
Rus test <sochiruss2@gmail.com>
2023-03-29 22:06:14 +0300

    Изменения в файле src/Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int currentBalance = 100; // начальная сумма на счету клиента
+        int refillAmount = 1250; // сумма пополнения
+
+        int bonus = 0; // инициализируем переменную бонуса нулем
+
+        if (refillAmount > 1000) { // если сумма пополнения больше 1000
+
+            bonus = (refillAmount - 1000) / 100; // рассчитываем количество бонусных рублей
+        }
+
+        int totalBalance = currentBalance + refillAmount + bonus; // рассчитываем итоговый баланс
+
+        System.out.println("Итоговый счёт: " + totalBalance + " рублей");
+        System.out.println("Бонусные рубли: " + bonus + " рублей");
+
     }
 }
